f5eac65f5dad412f20ad8edf0d859a1db96465ea
Sohaib <6sohaib@gmail.com>
2023-11-27 16:24:01 +0200

    Added Main Menu

---
+++ Assets/Scripts/Mechanics/MainMenuButtons.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    //public void OnPointerClick(PointerEventData eventData)
+    //{
+    //    Debug.Log("Clicked!"); // Placeholder action for demonstration
+    // Add your desired action or functionality here
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        Debug.Log("Clicked! on if");
+    //    }
+    // }
+    // This function will be called when the button is clicked
+    public void OnButtonClick()
+    {
+        Debug.Log("Button Clicked!"); // Placeholder action for demonstration
+        // Add your desired functionality here
+    }
+
+
+}
